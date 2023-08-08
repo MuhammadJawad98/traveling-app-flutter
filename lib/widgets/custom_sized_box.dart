845f14f9bbd001ae39b69045d48a86160957a641
Ch Muhammad Tahir <tahir.ctn001@gmail.com>
2023-08-08 13:11:43 +0500

    Custom Media Query, Custom_sized _Box

---
+++ lib/widgets/custom_sized_box.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomSizedBox extends StatelessWidget {
+  final Widget? child;
+  final double? height;
+  final double? width;
+  const CustomSizedBox({super.key, this.child, this.height, this.width});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      width: width,
+      child: child,
+    );
+  }
+}
