0a9edeff19c910f4ff7905989f26b5f29019a394
Abubakar009009 <140379717+Abubakar009009@users.noreply.github.com>
2023-08-08 20:24:05 +0500

    final commit of today

--- lib/views/attraction_details_page/attraction_details_page.dart
+++ lib/views/attraction_details_page/attraction_details_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:traveling_app_flutter/models/locations_model.dart';
+import 'package:traveling_app_flutter/widgets/custom_button.dart';
 import 'package:traveling_app_flutter/widgets/custom_text.dart';
 
 import '../../utils/media_query.dart';
@@ -22,7 +23,7 @@ class _AttractionDetailsPageState extends State<AttractionDetailsPage> {
     return Scaffold(
       body: Stack(
         children: [
-          Container(
+          SizedBox(
             height: GetScreenSize.getScreenHeight(context),
             width: GetScreenSize.getScreenWidth(context),
             child: Image.network(
@@ -57,26 +58,92 @@ class _AttractionDetailsPageState extends State<AttractionDetailsPage> {
                 fontWeight: FontWeight.w500),
           ),
           Positioned(
-            top: GetScreenSize.getScreenHeight(context) * 0.75,
+            top: GetScreenSize.getScreenHeight(context) * 0.80,
             left: GetScreenSize.getScreenWidth(context) * 0.07,
             right: GetScreenSize.getScreenWidth(context) * 0.05,
-            child: SizedBox(
-              height: 40,
-              child: RatingBar.builder(
-                initialRating: ratingValue,
-                minRating: 1,
-                direction: Axis.horizontal,
-                allowHalfRating: true,
-                itemCount: 5,
-                itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                itemSize: 30,
-                itemBuilder: (context, _) => Icon(
-                  Icons.star,
-                  color: Colors.amber,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 40,
+                  child: RatingBar.builder(
+                    initialRating: ratingValue,
+                    minRating: 1,
+                    direction: Axis.horizontal,
+                    allowHalfRating: true,
+                    itemCount: 5,
+                    itemPadding: const EdgeInsets.symmetric(horizontal: 2.0),
+                    itemSize: 16,
+                    itemBuilder: (context, _) => const Icon(
+                      Icons.star,
+                      color: Colors.amber,
+                    ),
+                    onRatingUpdate: (rating) {},
+                    unratedColor: Colors.grey[300],
+                  ),
                 ),
-                onRatingUpdate: (rating) {},
-                unratedColor: Colors.grey[300],
-              ),
+                const SizedBox(
+                  width: 10,
+                ),
+                CustomText(
+                    text: widget.data.rating,
+                    color: Colors.white,
+                    size: 14,
+                    maxline: 1,
+                    fontWeight: FontWeight.w400),
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: CustomText(
+                      text: '(${widget.data.numberReviews})',
+                      color: Colors.white,
+                      size: 14,
+                      maxline: 1,
+                      fontWeight: FontWeight.w400),
+                ),
+                const CustomText(
+                    text: 'See reviews',
+                    color: Colors.white,
+                    size: 14,
+                    maxline: 1,
+                    fontWeight: FontWeight.w400)
+              ],
+            ),
+          ),
+          Positioned(
+            top: GetScreenSize.getScreenHeight(context) * 0.85,
+            left: GetScreenSize.getScreenWidth(context) * 0.07,
+            right: GetScreenSize.getScreenWidth(context) * 0.05,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomTextButton(
+                  height: GetScreenSize.getScreenWidth(context) * 0.15,
+                  width: GetScreenSize.getScreenWidth(context) * 0.38,
+                  onTab: () {},
+                  buttonText: 'Enter the Plan ',
+                  buttonColor: const Color.fromRGBO(255, 255, 255, 0.1),
+                  radius: 41,
+                  fontSize: 16,
+                  fontColor: Colors.black,
+                ),
+                SizedBox(
+                  width: GetScreenSize.getScreenWidth(context) * 0.12,
+                ),
+                CustomTextButton(
+                  height: GetScreenSize.getScreenWidth(context) * 0.15,
+                  width: GetScreenSize.getScreenWidth(context) * 0.38,
+                  onTab: () {},
+                  buttonText: 'View Other',
+                  buttonColor: const Color.fromRGBO(255, 255, 255, 0.1),
+                  radius: 41,
+                  fontSize: 16,
+                  fontColor: Colors.black,
+                ),
+              ],
             ),
           ),
         ],
