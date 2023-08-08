9480921a960f9a8cb7551096012ec87117072394
Abubakar009009 <140379717+Abubakar009009@users.noreply.github.com>
2023-08-08 14:42:58 +0500

    front-page-commit

--- lib/Views/front_page.dart
+++ lib/Views/front_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/custom_text.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 class FrontPage extends StatefulWidget {
-  const FrontPage({super.key});
+  FrontPage({super.key});
 
   @override
   State<FrontPage> createState() => _FrontPageState();
@@ -10,27 +13,83 @@ class FrontPage extends StatefulWidget {
 class _FrontPageState extends State<FrontPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Padding(
-        padding: EdgeInsets.only(left: 25.0, top: 100),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Find your next trip',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.only(left: 25.0, top: 100),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Find your next trip',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            ),
-            Text(
-              'Nordic scenery',
-              style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.w600,
+              const Text(
+                'Nordic scenery',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Container(
+                    width: 250,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      border: Border.all(
+                        color: const Color.fromRGBO(233, 233, 233, 1),
+                        width: 1,
+                      ),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 0.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.search),
+                          hintText: 'Search',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    height: 52,
+                    width: 52,
+                    //color: Colors.amber,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.blue),
+
+                    child: const Icon(Icons.line_axis),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              const CustomText(
+                  color: Color.fromRGBO(0, 0, 0, 1),
+                  fontWeight: FontWeight.w600,
+                  maxline: 1,
+                  size: 18,
+                  text: 'Popular locations'),
+            ],
+          ),
         ),
       ),
     );
