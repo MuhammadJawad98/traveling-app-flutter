ee6c424ed4283191acee7bf8b6ceaa8d7e485517
talhaSaeed01 <talhasaeed152y@gmail.com>
2023-08-09 18:46:13 +0500

    44

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,51 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'package:provider/provider.dart';
+// import 'package:traveling_app_flutter/providers/locations_provider.dart';
+// import 'package:traveling_app_flutter/providers/sign_in_provider.dart';
+// import 'package:traveling_app_flutter/providers/sign_up_provider.dart';
+
+// import 'providers/provider_makeplan_page.dart';
+// import 'views/onboarding_page/splash_screen.dart';
+
+// void main(List<String> args) {
+//   runApp(MultiProvider(
+//       providers: [
+//         ChangeNotifierProvider<Counter>(create: (context) => Counter()),
+//         ChangeNotifierProvider<SignInProvider>(create: (context) => SignInProvider()),
+//         ChangeNotifierProvider<SignUpProvider>(create: (context) => SignUpProvider()),
+//         ChangeNotifierProvider<LocationsProvider>(create: (context) => LocationsProvider()),
+//       ],
+//       child: MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
+//         home: const SafeArea(child: SplashScreen()),
+//       )));
+// }
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:traveling_app_flutter/providers/locations_provider.dart';
 import 'package:traveling_app_flutter/providers/sign_in_provider.dart';
 import 'package:traveling_app_flutter/providers/sign_up_provider.dart';
+import 'package:device_preview/device_preview.dart'; // Import device_preview package
 
 import 'providers/provider_makeplan_page.dart';
 import 'views/onboarding_page/splash_screen.dart';
 
 void main(List<String> args) {
-  runApp(MultiProvider(
+  runApp(
+    DevicePreview(
+      builder: (context) => MyApp(), // Wrap your app with DevicePreview
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
       providers: [
         ChangeNotifierProvider<Counter>(create: (context) => Counter()),
         ChangeNotifierProvider<SignInProvider>(create: (context) => SignInProvider()),
@@ -17,8 +53,11 @@ void main(List<String> args) {
         ChangeNotifierProvider<LocationsProvider>(create: (context) => LocationsProvider()),
       ],
       child: MaterialApp(
+        builder: DevicePreview.appBuilder, // Enable device preview builder
         debugShowCheckedModeBanner: false,
         theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
         home: const SafeArea(child: SplashScreen()),
-      )));
+      ),
+    );
+  }
 }
