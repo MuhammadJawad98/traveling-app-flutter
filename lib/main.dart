bd98150a43ea28be8043d22292eb0c51842566a0
usmanjanjua1 <usman.tariq9200@gmail.com>
2023-08-11 10:50:04 +0500

    1st commit of 11 AUG

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:traveling_app_flutter/providers/Itinerary_provider.dart';
 import 'package:traveling_app_flutter/providers/locations_provider.dart';
 import 'package:traveling_app_flutter/providers/sign_in_provider.dart';
 import 'package:traveling_app_flutter/providers/sign_up_provider.dart';
 import 'package:traveling_app_flutter/views/itinerary_page/itinerary_screen_widget.dart';
+import 'package:traveling_app_flutter/views/weather_query_page/wearther_query_page.dart';
 
 import 'providers/provider_makeplan_page.dart';
 import 'views/onboarding_page/splash_screen.dart';
@@ -19,10 +21,12 @@ void main(List<String> args) {
             create: (context) => SignUpProvider()),
         ChangeNotifierProvider<LocationsProvider>(
             create: (context) => LocationsProvider()),
+        ChangeNotifierProvider<ItineraryProvider>(
+            create: (context) => ItineraryProvider())
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
-        home: SafeArea(child: ItineraryScreenWidget()),
+        home: SafeArea(child: WeatherQueryPage()),
       )));
 }
