7cb129d6a6c34ce46675728214c4c006cdd61459
Abubakar009009 <140379717+Abubakar009009@users.noreply.github.com>
2023-08-11 13:57:36 +0500

    final

--- lib/utils/app_strings.dart
+++ lib/utils/app_strings.dart
@@ -18,12 +18,6 @@ class AppString {
       "Here you can post your travel blog and \n promote your travel videos";
   static String yoursociallinkbottomtext =
       "Add to see if your social links are correct";
-  static String attractionBSpricing = "Pricing";
-  static String attractionBSreview = "See review";
-  static String attractionBSbuttontext = "Plan trip";
-  static String attractionDSreview = "See review";
-  static String attractionDSbuttonplan = "Enter the Plan ";
-  static String attractionDSbuttonview = "View Other";
 
   //--------------------Muhammad Tahir -----------------------------------
   static String btnContinue = "Continue";
@@ -81,6 +75,12 @@ class AppString {
   static const String findTripText = 'Find your next trip';
   static const String nordicscenery = 'Nordic scenery';
   static const String popularLocations = 'Popular locations';
+  static String attractionBSpricing = "Pricing";
+  static String attractionBSreview = "See review";
+  static String attractionBSbuttontext = "Plan trip";
+  static String attractionDSreview = "See review";
+  static String attractionDSbuttonplan = "Enter the Plan ";
+  static String attractionDSbuttonview = "View Other";
   //-------------------- Bhai Usman  -----------------------------------
   //transportation screen strings
   static const String nordinCottage = 'Nordic Cottage';
