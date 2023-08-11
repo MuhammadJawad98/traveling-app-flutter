d21d9b599fd6a63d3340542f614c4f3a2619ec12
usmanjanjua1 <usman.tariq9200@gmail.com>
2023-08-11 12:22:06 +0500

    final work
    complete

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:traveling_app_flutter/providers/sign_up_provider.dart';
 import 'package:traveling_app_flutter/views/itinerary_page/itinerary_screen_widget.dart';
 import 'package:traveling_app_flutter/views/onboarding_page/newplan_page.dart';
 import 'package:traveling_app_flutter/views/onboarding_page/splash_screen.dart';
+import 'package:traveling_app_flutter/views/weather_query_page/wearther_query_page.dart';
 import 'providers/provider_makeplan_page.dart';
 
 void main(List<String> args) {
@@ -27,7 +28,7 @@ void main(List<String> args) {
           builder: DevicePreview.appBuilder,
           debugShowCheckedModeBanner: false,
           theme: ThemeData(fontFamily: 'Poppins'),
-          home: SafeArea(child: ItineraryScreenWidget()),
+          home: SafeArea(child: WeatherQueryPage()),
         ),
       )));
 }
--- lib/views/weather_query_page/wearther_query_page.dart
+++ lib/views/weather_query_page/wearther_query_page.dart
@@ -28,83 +28,116 @@ class _WeatherQueryPageState extends State<WeatherQueryPage> {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-        backgroundColor: AppColors.containerColor,
-        body: Column(
-          children: [
-            Container(
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    const MyWeatherAppBar(title: AppString.itineraryForm),
-                    GroupButton(
-                      labels: const [
-                        AppString.island,
-                        AppString.beach,
-                        AppString.resort
-                      ],
-                      onPressed: (index) {},
-                    ),
-                    CustomSizedBox(
-                      height: GetScreenSize.getScreenWidth(context) * 0.02,
-                    ),
-                    const TabBar(
-                      indicatorColor: Colors.blue,
-                      indicatorWeight: 3,
-                      tabs: [
-                        DayTab(title1: AppString.day1, title2: AppString.date1),
-                        DayTab(title1: AppString.day2, title2: AppString.date2),
-                        DayTab(title1: AppString.day3, title2: AppString.date3),
-                      ],
-                    ),
-                  ],
-                )),
-            Container(
-              height: GetScreenSize.getScreenWidth(context) * 1,
-              color: AppColors.containerColor,
-              //height: double.infinity,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Consumer<ItineraryProvider>(
-                    builder: (context, provier, child) {
-                  return ListView.builder(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      shrinkWrap: true,
-                      itemCount: 5,
-                      itemBuilder: (context, index) {
-                        if (index == 0) {
-                          isFirst = true;
-                          isLast = false;
-                          isPast = true;
-                        } else {
-                          isFirst = false;
-                          isPast = false;
-                        }
-                        if (index == provier.locations.length - 1) {
-                          isLast = true;
-                        }
-                        return CustomTimelineTile(
-                            isFirst: isFirst,
-                            isLast: isLast,
-                            isPast: isPast,
-                            location: provier.locations[index]);
-                      });
-                }),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: GetScreenSize.getScreenWidth(context) * 0.065),
-              child: CustomTextButton(
-                  height: 50,
-                  width: double.maxFinite,
-                  onTab: () {},
-                  buttonText: AppString.viewSpecificItinerary,
-                  buttonColor: Colors.blue,
-                  radius: 30,
-                  fontSize: 16),
-            ),
-          ],
+        bottomNavigationBar: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: GetScreenSize.getScreenWidth(context) * 0.065,
+              vertical: GetScreenSize.getScreenWidth(context) * 0.02),
+          child: CustomTextButton(
+              height: 50,
+              width: double.maxFinite,
+              onTab: () {},
+              buttonText: AppString.viewSpecificItinerary,
+              buttonColor: Colors.blue,
+              radius: 30,
+              fontSize: 16),
         ),
+
+        backgroundColor: AppColors.containerColor,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      const MyWeatherAppBar(title: AppString.itineraryForm),
+                      GroupButton(
+                        labels: const [
+                          AppString.island,
+                          AppString.beach,
+                          AppString.resort
+                        ],
+                        onPressed: (index) {},
+                      ),
+                      CustomSizedBox(
+                        height: GetScreenSize.getScreenWidth(context) * 0.02,
+                      ),
+                      const TabBar(
+                        indicatorColor: Colors.blue,
+                        indicatorWeight: 3,
+                        tabs: [
+                          DayTab(
+                              title1: AppString.day1, title2: AppString.date1),
+                          DayTab(
+                              title1: AppString.day2, title2: AppString.date2),
+                          DayTab(
+                              title1: AppString.day3, title2: AppString.date3),
+                        ],
+                      ),
+                    ],
+                  )),
+              SizedBox(
+                height: GetScreenSize.getScreenWidth(context) * 1.3,
+                child: TabBarView(children: [
+                  timelineView(context),
+                  timelineView(context),
+                  timelineView(context),
+                ]),
+              )
+            ],
+          ),
+        ),
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {},
+        //   child: Padding(
+        //     padding: EdgeInsets.symmetric(
+        //         horizontal: GetScreenSize.getScreenWidth(context) * 0.065,
+        //         vertical: GetScreenSize.getScreenWidth(context) * 0.02),
+        //     child: CustomTextButton(
+        //         height: 50,
+        //         width: double.maxFinite,
+        //         onTab: () {},
+        //         buttonText: AppString.viewSpecificItinerary,
+        //         buttonColor: Colors.blue,
+        //         radius: 30,
+        //         fontSize: 16),
+        //   ),
+        // ),
+      ),
+    );
+  }
+
+  Widget timelineView(BuildContext context) {
+    return Container(
+      height: GetScreenSize.getScreenWidth(context) * 1.3,
+      color: AppColors.containerColor,
+      //height: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Consumer<ItineraryProvider>(builder: (context, provier, child) {
+          return ListView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              //shrinkWrap: true,
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                if (index == 0) {
+                  isFirst = true;
+                  isLast = false;
+                  isPast = true;
+                } else {
+                  isFirst = false;
+                  isPast = false;
+                }
+                if (index == provier.locations.length - 1) {
+                  isLast = true;
+                }
+                return CustomTimelineTile(
+                    isFirst: isFirst,
+                    isLast: isLast,
+                    isPast: isPast,
+                    location: provier.locations[index]);
+              });
+        }),
       ),
     );
   }
