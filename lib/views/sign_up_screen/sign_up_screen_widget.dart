38e6e9b07e144be63d4377bb67aa112179b41f7a
talhaSaeed01 <talhasaeed152y@gmail.com>
2023-08-15 15:23:44 +0500

    ok

--- lib/views/sign_up_screen/sign_up_screen_widget.dart
+++ lib/views/sign_up_screen/sign_up_screen_widget.dart
@@ -25,19 +25,14 @@ class _SignUpScreenWidgetState extends State<SignUpScreenWidget> {
   final TextEditingController emailTfController = TextEditingController();
   final TextEditingController passwordTfController = TextEditingController();
 
-  final TextEditingController confirmPasswordTfController =
-      TextEditingController();
+  final TextEditingController confirmPasswordTfController = TextEditingController();
 
   @override
   void initState() {
-    Provider.of<SignUpProvider>(context, listen: false)
-        .cheeckVaidEmail(emailTfController);
-    Provider.of<SignUpProvider>(context, listen: false)
-        .cheeckVaidPassword(passwordTfController);
+    Provider.of<SignUpProvider>(context, listen: false).cheeckVaidEmail(emailTfController);
+    Provider.of<SignUpProvider>(context, listen: false).cheeckVaidPassword(passwordTfController);
     super.initState();
-    Provider.of<SignUpProvider>(context, listen: false)
-        .checkConfirmPasswordPassword(
-            passwordTfController, confirmPasswordTfController);
+    Provider.of<SignUpProvider>(context, listen: false).checkConfirmPasswordPassword(passwordTfController, confirmPasswordTfController);
     super.initState();
   }
 
@@ -52,13 +47,11 @@ class _SignUpScreenWidgetState extends State<SignUpScreenWidget> {
 
   @override
   Widget build(BuildContext context) {
+    //
     double width1 = GetScreenSize.getScreenWidth(context);
-    AppCommonFunctions.printLogs(
-        "getScreenSize: ${GetScreenSize.getScreenSize(context)}");
-    AppCommonFunctions.printLogs(
-        "getScreenHeight: ${GetScreenSize.getScreenHeight(context)}");
-    AppCommonFunctions.printLogs(
-        "getScreenWidth: ${GetScreenSize.getScreenWidth(context)}");
+    AppCommonFunctions.printLogs("getScreenSize: ${GetScreenSize.getScreenSize(context)}");
+    AppCommonFunctions.printLogs("getScreenHeight: ${GetScreenSize.getScreenHeight(context)}");
+    AppCommonFunctions.printLogs("getScreenWidth: ${GetScreenSize.getScreenWidth(context)}");
     double buttonWidth = GetScreenSize.getScreenWidth(context) * 0.8;
     final double buttonHeith = GetScreenSize.getScreenWidth(context) * 0.12;
     return SafeArea(
@@ -71,115 +64,82 @@ class _SignUpScreenWidgetState extends State<SignUpScreenWidget> {
                   left: width1 * 0.08,
                   right: width1 * 0.08,
                 ),
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      const Expanded(child: CustomSizedBox()),
-                      CustomIconButton(
-                          iconData: Icons.arrow_back,
-                          onTab: () {
-                            Navigator.pop(context);
-                          },
-                          size: width1 * 0.065),
-                      // CustomSizedBox(height: width1 * 0.05),
-                      const Expanded(child: CustomSizedBox()),
-                      CustomText(
-                          text: AppString.signUpfreeText,
-                          color: Colors.black,
-                          size: GetScreenSize.getScreenWidth(context) * 0.07,
-                          maxline: 1,
-                          fontWeight: FontWeight.w600),
-                      CustomSizedBox(height: width1 * 0.05),
-                      CustomText(
-                          text: AppString.emailText,
-                          color: AppColors.screenTextColor,
-                          size: width1 * 0.03,
-                          maxline: 1,
-                          fontWeight: FontWeight.w400),
-                      CustomSizedBox(height: width1 * 0.02),
-                      CustomTextField(
-                        controller: emailTfController,
-                        hint: AppString.hintEmailText,
-                        raduis: 36,
-                        prefixIcon: Image.asset(AppAssets.perosn),
-                        erroText: AppString.invalidEmail,
-                        isValid:
-                            Provider.of<SignUpProvider>(context, listen: true)
-                                .isValidEmail,
-                      ),
-                      CustomSizedBox(height: width1 * 0.05),
-                      CustomText(
-                          text: AppString.passwordText,
-                          color: AppColors.screenTextColor,
-                          size: width1 * 0.03,
-                          maxline: 1,
-                          fontWeight: FontWeight.w400),
-                      CustomSizedBox(height: width1 * 0.02),
-                      CustomTextField(
-                        controller: passwordTfController,
-                        hint: AppString.hintPasswordText,
-                        raduis: 36,
-                        obsecure: true,
-                        prefixIcon: Image.asset(AppAssets.lock),
-                        erroText: AppString.invalidPasswordHint,
-                        isValid:
-                            Provider.of<SignUpProvider>(context, listen: true)
-                                .isValidPassword,
-                      ),
-                      CustomSizedBox(height: width1 * 0.05),
-                      CustomText(
-                          text: AppString.confirmPasswordText,
-                          color: AppColors.screenTextColor,
-                          size: width1 * 0.03,
-                          maxline: 1,
-                          fontWeight: FontWeight.w400),
-                      CustomSizedBox(height: width1 * 0.02),
-                      CustomTextField(
-                          controller: confirmPasswordTfController,
-                          hint: AppString.hintConfirmPasswordText,
-                          raduis: 36,
-                          prefixIcon: Image.asset(AppAssets.perosn)),
-                      CustomSizedBox(height: width1 * 0.03),
-                      Row(children: [
-                        Image.asset(AppAssets.richTexticon,
-                            width: width1 * 0.04),
-                        const CustomSizedBox(width: 10),
-                        Expanded(
-                          child: CustomRichText(
-                              text: AppString.termsOfServicesPart1,
-                              defaultStyle: TextStyle(
-                                color: Colors.black,
-                                fontSize: width1 * 0.03,
-                              ),
-                              textSpans: [
-                                TextSpan(
-                                    text: AppString.termsOfServicesPart2,
-                                    style: const TextStyle(
-                                      color: AppColors.blueButton,
-                                    ))
-                              ]),
-                        )
-                      ]),
-                      Expanded(child: CustomSizedBox()),
-                      Align(
-                          alignment: Alignment.bottomCenter,
-                          child: CustomTextButton(
-                            buttonColor: AppColors.blueButton,
-                            height: buttonHeith,
-                            onTab: () {
-                              onTabConfirm();
-                            },
-                            radius: 36,
-                            buttonText: AppString.btnContinue,
-                            width: buttonWidth,
-                            fontSize:
-                                GetScreenSize.getScreenWidth(context) * 0.05,
-                          )),
-                      const Expanded(
-                        child: CustomSizedBox(),
-                      )
-                    ]),
+                child: Column(crossAxisAlignment: CrossAxisAlignment.start, mainAxisSize: MainAxisSize.max, children: [
+                  const Expanded(child: CustomSizedBox()),
+                  CustomIconButton(
+                      iconData: Icons.arrow_back,
+                      onTab: () {
+                        Navigator.pop(context);
+                      },
+                      size: width1 * 0.065),
+                  // CustomSizedBox(height: width1 * 0.05),
+                  const Expanded(child: CustomSizedBox()),
+                  CustomText(text: AppString.signUpfreeText, color: Colors.black, size: GetScreenSize.getScreenWidth(context) * 0.07, maxline: 1, fontWeight: FontWeight.w600),
+                  CustomSizedBox(height: width1 * 0.05),
+                  CustomText(text: AppString.emailText, color: AppColors.screenTextColor, size: width1 * 0.03, maxline: 1, fontWeight: FontWeight.w400),
+                  CustomSizedBox(height: width1 * 0.02),
+                  CustomTextField(
+                    controller: emailTfController,
+                    hint: AppString.hintEmailText,
+                    raduis: 36,
+                    prefixIcon: Image.asset(AppAssets.perosn),
+                    erroText: AppString.invalidEmail,
+                    isValid: Provider.of<SignUpProvider>(context, listen: true).isValidEmail,
+                  ),
+                  CustomSizedBox(height: width1 * 0.05),
+                  CustomText(text: AppString.passwordText, color: AppColors.screenTextColor, size: width1 * 0.03, maxline: 1, fontWeight: FontWeight.w400),
+                  CustomSizedBox(height: width1 * 0.02),
+                  CustomTextField(
+                    controller: passwordTfController,
+                    hint: AppString.hintPasswordText,
+                    raduis: 36,
+                    obsecure: true,
+                    prefixIcon: Image.asset(AppAssets.lock),
+                    erroText: AppString.invalidPasswordHint,
+                    isValid: Provider.of<SignUpProvider>(context, listen: true).isValidPassword,
+                  ),
+                  CustomSizedBox(height: width1 * 0.05),
+                  CustomText(text: AppString.confirmPasswordText, color: AppColors.screenTextColor, size: width1 * 0.03, maxline: 1, fontWeight: FontWeight.w400),
+                  CustomSizedBox(height: width1 * 0.02),
+                  CustomTextField(controller: confirmPasswordTfController, hint: AppString.hintConfirmPasswordText, raduis: 36, prefixIcon: Image.asset(AppAssets.perosn)),
+                  CustomSizedBox(height: width1 * 0.03),
+                  Row(children: [
+                    Image.asset(AppAssets.richTexticon, width: width1 * 0.04),
+                    const CustomSizedBox(width: 10),
+                    Expanded(
+                      child: CustomRichText(
+                          text: AppString.termsOfServicesPart1,
+                          defaultStyle: TextStyle(
+                            color: Colors.black,
+                            fontSize: width1 * 0.03,
+                          ),
+                          textSpans: [
+                            TextSpan(
+                                text: AppString.termsOfServicesPart2,
+                                style: const TextStyle(
+                                  color: AppColors.blueButton,
+                                ))
+                          ]),
+                    )
+                  ]),
+                  Expanded(child: CustomSizedBox()),
+                  Align(
+                      alignment: Alignment.bottomCenter,
+                      child: CustomTextButton(
+                        buttonColor: AppColors.blueButton,
+                        height: buttonHeith,
+                        onTab: () {
+                          onTabConfirm();
+                        },
+                        radius: 36,
+                        buttonText: AppString.btnContinue,
+                        width: buttonWidth,
+                        fontSize: GetScreenSize.getScreenWidth(context) * 0.05,
+                      )),
+                  const Expanded(
+                    child: CustomSizedBox(),
+                  )
+                ]),
               ))),
     );
   }
@@ -197,8 +157,7 @@ class _SignUpScreenWidgetState extends State<SignUpScreenWidget> {
     if (confirmPassword.isEmpty) {
       AppCommonFunctions.showToast(AppString.emptyConfirmPasswordText, context);
     } else {
-      Navigator.push(context,
-          MaterialPageRoute(builder: (context) => SignInPageScreenWidget()));
+      Navigator.push(context, MaterialPageRoute(builder: (context) => SignInPageScreenWidget()));
     }
   }
 }
