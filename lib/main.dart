d8957ed19949b0b13e9da928ea5bf31f464f9549
Ch Muhammad Tahir <tahir.ctn001@gmail.com>
2023-08-15 15:23:09 +0500

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+import 'package:traveling_app_flutter/providers/Itinerary_provider.dart';
 import 'package:traveling_app_flutter/providers/bottom_sheet_provider.dart';
+import 'package:traveling_app_flutter/providers/day_task_providers/day1_task_provider.dart';
+import 'package:traveling_app_flutter/providers/day_task_providers/day2_task_provider.dart';
+import 'package:traveling_app_flutter/providers/day_task_providers/day3_task_provider.dart';
 import 'package:traveling_app_flutter/providers/locations_provider.dart';
 import 'package:traveling_app_flutter/providers/sign_in_provider.dart';
 import 'package:traveling_app_flutter/providers/sign_up_provider.dart';
@@ -28,10 +32,22 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<Counter>(create: (context) => Counter()),
-        ChangeNotifierProvider<SignInProvider>(create: (context) => SignInProvider()),
-        ChangeNotifierProvider<SignUpProvider>(create: (context) => SignUpProvider()),
-        ChangeNotifierProvider<LocationsProvider>(create: (context) => LocationsProvider()),
-        ChangeNotifierProvider<BottomSheetStateProvider>(create: (context) => BottomSheetStateProvider()),
+        ChangeNotifierProvider<SignInProvider>(
+            create: (context) => SignInProvider()),
+        ChangeNotifierProvider<SignUpProvider>(
+            create: (context) => SignUpProvider()),
+        ChangeNotifierProvider<LocationsProvider>(
+            create: (context) => LocationsProvider()),
+        ChangeNotifierProvider<BottomSheetStateProvider>(
+            create: (context) => BottomSheetStateProvider()),
+        ChangeNotifierProvider<Day1TaskProvider>(
+            create: (context) => Day1TaskProvider()),
+        ChangeNotifierProvider<Day2TaskProvider>(
+            create: (context) => Day2TaskProvider()),
+        ChangeNotifierProvider<Day3TaskProvider>(
+            create: (context) => Day3TaskProvider()),
+        ChangeNotifierProvider<ItineraryProvider>(
+            create: (context) => ItineraryProvider()),
       ],
       child: MaterialApp(
         // builder: DevicePreview.appBuilder, // Enable device preview builder
